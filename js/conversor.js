5992f7fe326a5173ffa35bafb7a82d4fc5883e30
Luis Cabrera Benito <luiscabrerabenito970621@gmail.com>
2016-01-28 18:21:20 -0600

    Ahora acepta unidades con espacios, por ejemplo "libra por pulgada cuadrada"

--- js/conversor.js
+++ js/conversor.js
@@ -104,49 +104,54 @@ function principal() {
 }
 function compruebaValores(oracion) {
     if (oracion) {
-        var arreglo = oracion.split(' ');
-        if (arreglo.length === 4) {
-            if (arreglo[arreglo.length - 1]) {
-                var primerNumero = arreglo[0],
-                    primeraUnidad = arreglo[1].toLowerCase(),
-                    palabraIntermedia = arreglo[2].toLowerCase(),
-                    segundaUnidad = arreglo[3].toLowerCase();
-                if (palabrasPermitidas.indexOf(palabraIntermedia) !== -1) {
-                    if (!isNaN(primerNumero)) {
-                        if (primerNumero.length <= limiteLongitudNumeros - 1) {
-                            if (primeraUnidad.slice(-1) === 's') {
-                                primeraUnidad = primeraUnidad.slice(0, -1);
-                            }
-                            if (segundaUnidad.slice(-1) === 's') {
-                                segundaUnidad = segundaUnidad.slice(0, -1);
-                            }
-                            var grupoUnidad1 = dameGrupo(primeraUnidad),
-                                grupoUnidad2 = dameGrupo(segundaUnidad);
-                            if (grupoUnidad1 !== false) {
-                                if (grupoUnidad2 !== false) {
-                                    if (grupoUnidad1 === grupoUnidad2) {
-                                        convierte(grupoUnidad1, grupoUnidad2, primerNumero, primeraUnidad, segundaUnidad);
-                                    } else {
-                                        escribeError('Los grupos de unidades no son compatibles: ' + primeraUnidad + ' y ' + segundaUnidad);
-                                    }
-
+        var oracionCortada = cortaOracion(oracion);
+        if (isNaN(oracionCortada)) {
+            var numero = oracionCortada.numero,
+                unidad1 = oracionCortada.u1.toLowerCase(),
+                nexo = oracionCortada.nexo.toLowerCase(),
+                unidad2 = oracionCortada.u2.toLowerCase();
+            if (!isNaN(numero)) {
+                if (numero.length <= limiteLongitudNumeros - 1) {
+                    if (unidad1.slice(-1) === 's') {
+                        unidad1 = unidad1.slice(0, -1);
+                    }
+                    if (unidad2.slice(-1) === 's') {
+                        unidad2 = unidad2.slice(0, -1);
+                    }
+                    var grupoUnidad1 = dameGrupo(unidad1),
+                        grupoUnidad2 = dameGrupo(unidad2);
+                    if (grupoUnidad1 !== false) {
+                        if (grupoUnidad2 !== false) {
+                            if (grupoUnidad1 === grupoUnidad2) {
+                                convierte(grupoUnidad1, grupoUnidad2, numero, unidad1, unidad2);
                                 } else {
-                                    escribeError('La segunda unidad no existe: ' + segundaUnidad);
+                                escribeError('Los grupos de unidades no son compatibles: ' + unidad1 + ' y ' + unidad2);
                                 }
                             } else {
-                                escribeError('La primera unidad no existe: ' + primeraUnidad);
+                            escribeError('La segunda unidad no existe: ' + unidad2);
                             }
                         } else {
-                            escribeError('Lo siento, solamente se permiten números de 16 cifras sin incluir el punto decimal');
+                        escribeError('La primera unidad no existe: ' + unidad1);
                         }
                     } else {
-                        escribeError('La primera parte de la oración no es un número: ' + primerNumero);
+                    escribeError('Lo siento, solamente se permiten números de 16 cifras sin incluir el punto decimal');
                     }
                 } else {
-                    escribeError('Trata de unir con otra palabra como "en" o "a": ' + palabraIntermedia);
+                escribeError('La primera parte de la oración no es un número: ' + numero);
                 }
+
+        } else {
+            switch (oracionCortada) {
+                case 0:
+                    escribeError('No entiendo lo que quieres. Recuerda que debes unir con "en" o "a", por ejemplo "1 milla a metros"');
+                    break;
+                case 1:
+                    escribeError('No entiendo lo que quieres. Recuerda que no debes incluir espacios al inicio');
+                    break;
             }
         }
+    } else {
+        $('.ocultable').hide();
     }
 }
 function dameGrupo(unidad) {
@@ -160,7 +165,37 @@ function dameGrupo(unidad) {
 function dameEquivalencia(grupo, u1, u2) {
     return unidades[grupo].equivalencias[u1][u2];
 }
+function cortaOracion(o) {
+    var posicionPrimerNumero = o.indexOf(" ");
+    if (posicionPrimerNumero > 0) {
+        var primerNumero = o.slice(0, posicionPrimerNumero);
+        var posicionNexo;
+        var nexo;
+        var x;
+        for (x in palabrasPermitidas) {
+            posicionNexo = o.indexOf(" " + palabrasPermitidas[x] + " ", posicionPrimerNumero + 1);
+            if (posicionNexo !== -1) {
+                break;
+            }
+        }
+        if (posicionNexo !== -1) {
+            nexo = palabrasPermitidas[x];
+            var unidad1 = o.slice(posicionPrimerNumero + 1, posicionNexo);
+            var unidad2 = o.slice(posicionNexo + 2 + nexo.length, o.length);
+            return {
+                'numero': primerNumero,
+                'nexo': nexo,
+                'u1': unidad1,
+                'u2': unidad2
+            };
+        } else {
+            return 0;
+        }
+    } else {
+        return 1;
+    }
 
+}
 function cambiaPlaceholder() {
     if (contador < sugerencias.length) {
         $inputPrincipal.attr('placeholder', sugerencias[contador]);
@@ -195,7 +230,6 @@ function escribeResultado(numero, u1, u2, resultado) {
     $resultado.html(mensaje);
     $('.ocultable').show();
 }
-
 function llenaSelectUnidades(selector) {
     for (var opcion in unidades) {
         selector.append($('<option>', {
@@ -223,11 +257,9 @@ function llenaSegundoGrupo(selector, grupo) {
         }));
     }
 }
-
 function aLetraCapital(palabra) {
     return palabra[0].toUpperCase() + palabra.slice(1, palabra.length);
 }
-
 function convierte(grupo, grupo2, numero, u1, u2) {
     var resultado = dameEquivalencia(grupo, u1, u2) * numero;
     llenaPrimerGrupo($selectPrimerGrupo, grupo);
@@ -238,19 +270,5 @@ function convierte(grupo, grupo2, numero, u1, u2) {
     $selectPrimerGrupo.val(u1);
     $selectSegundoGrupo.val(u2);
     escribeResultado(numero, u1, u2, resultado);
-}
-function prueba(abreviacion) {
-    for (var ge in abreviaciones) {
-        var x = abreviaciones[ge];
-        for (var y in x) {
-            var xx = abreviaciones[ge][y];
-            var gegege = xx.indexOf(abreviacion);
-            if (gegege !== -1) {
-                console.log(xx);
-                console.log(abreviaciones[ge][y]);
-                break;
-            }
-        }
-    }
 }
 
